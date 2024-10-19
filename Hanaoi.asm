ff085fc302b43793fccc303ffc682e80b67dd556
Isaac Noriega <isaac.noriega@iteso.mx>
2024-10-18 18:51:33 -0600

    Config de torres , registros y creacion de discos en torre origen

--- Hanaoi.asm
+++ Hanaoi.asm
@@ -1,84 +1,35 @@
 .data
-        #Almacena la posicion de los discos (3 en este ejemplo)
-        discos: .word 0 , 0 , 0
-        mensaje: .asciiz "Mover disco de la torre "  # Mensaje que se imprimirá al mover un disco
-	    salto:   .asciiz "\n"                        # Salto de línea para formatear la salida
+
+    tower_storage: # Memoria de las torres
+
+    # s0 Numero de discos
+    # s1 Torre Origen
+    # s2 Torre Auxiliar
+    # s3 Torre Destino
+
 .text
+
+    #Declaracion de los registros
+    _start:
+        addi s0 , zero , 3 # Numeros de discos (Se puede cambiar el valor por N cantidad)
+
+        #Calculo de direcciones de cada Torre
+        addi t0 , zero , 4
+        mul t1 , s0 , t0 # Direcciones a reservar (N*4)
+        addi t1 , t1 , 4
+        lui s1 , %hi(tower_storage)
+        addi s1 , s1 , %lo(tower_storage) # Origen
+        add s2 , s1 , t1 # Auxiliar 
+        add s3 , s2 ,t1 # Destino
+
     main:
 
-        #Inicializamos el numero de discos
-        li s0 , 3
+        create_disc: # Torre origen
+            sw s2 ,0(s1) # Designamos la direccion como disco
+            addi s1 , s1 , 4 # Recorremos al a sig direccion y guardamos el disco
+            addi t2 , t2 , -1 # Restamos en 1 el valor de conteo
+            bgt t2 , zero , create_disc # Si el conteo es mayor a 0 se crean mas discos
+            lui s1 , %hi(tower_storage) # Cargamos la parte mas sognificativa
 
-        #Inicializamos las Torres
-        li s1 , 1 #Torre origen(A)
-        li s2 , 3 #Torre destino(C)
-        li s3 , 2 #Torre auxiliar(B)
-
-        addi a0 , s0 , 0  # a0 = numero de discos    
-        addi a1 , s1 , 0  # a1 = torre origen
-        addi a2 , s2 , 0  # a2 = torre destino
-        addi a3 , s3 , 0  # a3 = torre auxiliar
-        jal ra , hanoi # Funcion recursiva
-
-        # Finaliza el programa
-        li a7, 10 # Syscall de fin de programa
-        ecall
-
-        hanoi:
-
-            addi sp , sp , -16 # Reservamos 4 espacios del stack
-            sw ra , 12(sp) # Guardamos ra
-            sw a0 , 8(sp) #Guardamos los numeros de discos
-            sw a1 , 4(sp) #Guardamos torre origen
-            sw a2 , 0(sp) #Guardamos torre destino
-
-            li t0 , 1 # t0 = 1
-            beq a0 , t0 , mover_disco
-
-            #Movemos N-1 de origen a auxiliar
-            addi a0 , a0 , -1 # a0 = n - 1
-            addi a2 , a3 , 0 # Intercambiamos la torre destino con la auxiliar
-            jal ra , hanoi # Llamada recursiva
-
-            #Movemos el disco mas grande a Torre Origen
-            addi a0 , a0 , 1 #Restauramos el valor de N
-            jal ra , mover_disco # Mover disco grande
-
-            #Movemos los N - 1 discos de auxiliar a destino
-            addi a0 , a0 , -1 # a0 = n - 1
-            addi a2 , a3 , 0 # Intercambiamos la torre origen con la auxiliar
-            jal ra , hanoi # Llamada recursiva
-
-            # Restauramos el stack
-            lw ra , 12(sp) # Leemos el valor del ra
-            lw a0 , 8(sp) # Leemos el numero de discos
-            lw a1 , 4(sp) # Leemos la torre origen
-            lw a2 , 0(sp) # Leemos la torre destino
-            addi sp , sp , 16 # Liberamos el stack
-            ret
-        
-
-        mover_disco:
-            la a0, mensaje    # Cargar el mensaje "Mover disco de la torre "
-            li a7, 4          # syscall de imprimir string
-            ecall
-
-            addi a0, a1, 0     # Cargamos la torre origen en a0
-            li a7, 1          # syscall de imprimir integer
-            ecall
-
-            li a0, 'a'
-            li a7, 11         # syscall de imprimir char
-            ecall
-
-            addi a0, a2, 0     # Cargamos la torre destino en a0
-            li a7, 1          # syscall de imprimir integer
-            ecall
-
-            la a0, salto      # Cargar el salto de línea
-            li a7, 4          # syscall de imprimir string
-            ecall
-            
-            jr ra             # Retornar a la función recursiva
 
 
